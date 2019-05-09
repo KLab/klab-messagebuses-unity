c56c08caac2b534f88def055461a20f534aa784e
g-a-n-d-i <ganfulo@gmail.com>
2019-05-09 10:26:50 +0900

    Combine source files

--- Runtime/KLab/MessageBuses/MessageBus.cs
+++ Runtime/KLab/MessageBuses/MessageBus.cs
@@ -10,6 +10,83 @@ using System.Collections.Generic;
 
 namespace KLab.MessageBuses
 {
+    /// <summary>
+    /// Tagging Interface for signal message buses
+    /// </summary>
+    public interface IMessageBus { }
+
+
+    #region Options
+
+    /// <summary>
+    /// Additional options for message bus declaration
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+    public sealed class MessageBusOptionsAttribute : Attribute
+    {
+        /// <summary>
+        /// Connections container capacity
+        /// </summary>
+        public readonly int ConnectionsCapacity;
+
+        /// <summary>
+        /// Addresses container capacity
+        /// </summary>
+        public readonly int AddressesCapacity;
+
+        /// <summary>
+        /// Message container capacity
+        /// </summary>
+        public readonly int MessagesCapacity;
+
+
+        #region Ctors
+
+        /// <summary>
+        /// Initializes instance
+        /// </summary>
+        /// <param name="connectionsCapacity">Connections capacity</param>
+        /// <param name="addressesCapacity">Addresses capacity</param>
+        /// <param name="messagesCapacity">Messages capacity</param>
+        public MessageBusOptionsAttribute(int connectionsCapacity = 4, int addressesCapacity = 8, int messagesCapacity = 4)
+        {
+            ConnectionsCapacity = connectionsCapacity;
+            AddressesCapacity = addressesCapacity;
+            MessagesCapacity = messagesCapacity;
+        }
+
+        #endregion
+    }
+
+
+    #region Helpers
+
+    /// <summary>
+    /// Helper methods for <see cref="MessageBusOptionsAttribute"/>
+    /// </summary>
+    internal static class MessageBusOptions
+    {
+        /// <summary>
+        /// Gets options for type
+        /// </summary>
+        /// <param name="type">Type</param>
+        /// <returns>User options if available; default options otherwise</returns>
+        public static MessageBusOptionsAttribute GetForType(Type type)
+        {
+            var options = type.GetCustomAttributes(typeof(MessageBusOptionsAttribute), false);
+
+
+            return (options.Length > 0)
+                ? (options[0] as MessageBusOptionsAttribute)
+                : new MessageBusOptionsAttribute();
+        }
+    }
+
+    #endregion
+
+    #endregion
+
+
     #region Signal Buses
 
     #region Basic
